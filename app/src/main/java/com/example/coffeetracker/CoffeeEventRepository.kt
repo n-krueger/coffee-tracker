6c5899688e624875e0e1f9d119330437884ac2f6
Nils Krüger <36828049+n-krueger@users.noreply.github.com>
2020-11-14 17:29:49 +0100

    Add bar chart for current week

--- app/src/main/java/com/example/coffeetracker/CoffeeEventRepository.kt
+++ app/src/main/java/com/example/coffeetracker/CoffeeEventRepository.kt
@@ -6,6 +6,9 @@ import org.threeten.bp.OffsetDateTime
 class CoffeeEventRepository(private val coffeeEventDao: CoffeeEventDao) {
     val allCoffeeEvents: LiveData<List<CoffeeEvent>> = coffeeEventDao.getAll()
 
+    fun loadAllByTimestamp(start: OffsetDateTime, end: OffsetDateTime): LiveData<List<CoffeeEvent>> =
+        coffeeEventDao.loadAllByTimestamp(start, end)
+
     fun countByTimestamp(start: OffsetDateTime, end: OffsetDateTime): LiveData<Int> =
         coffeeEventDao.countByTimestamp(start, end)
 
--- app/src/main/java/com/example/coffeetracker/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/coffeetracker/ui/dashboard/DashboardFragment.kt
@@ -9,6 +9,21 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.coffeetracker.R
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.components.AxisBase
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.ValueFormatter
+
+private class WeekDayValueFormatter : ValueFormatter() {
+    private val days = arrayOf("Mo", "Tu", "Wed", "Th", "Fr", "Sa", "Su")
+
+    override fun getAxisLabel(value: Float, axis: AxisBase?): String {
+        return days.getOrNull(value.toInt()) ?: value.toString()
+    }
+}
 
 class DashboardFragment : Fragment() {
 
@@ -22,10 +37,22 @@ class DashboardFragment : Fragment() {
         dashboardViewModel =
             ViewModelProvider(this).get(DashboardViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+
+        val barChart: BarChart = root.findViewById(R.id.chart)
+
+        barChart.xAxis.valueFormatter = WeekDayValueFormatter()
+        barChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
+        barChart.axisRight.isEnabled = false
+
+        dashboardViewModel.coffeeCountsByDay.observe(viewLifecycleOwner, Observer {
+            val barDataList = it.map { (weekDay, count) ->
+                BarEntry(weekDay.toFloat(), count.toFloat())
+            }
+            val barDataSet: BarDataSet = BarDataSet(barDataList, "Coffees/Day");
+            barChart.data = BarData(barDataSet);
+            barChart.invalidate();
         })
+
         return root
     }
 }
